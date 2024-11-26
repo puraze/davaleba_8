0d54d481c3ed713053d257782ec2cb3f226c9096
puraze <Alexandremiqadze@gmail.com>
2024-11-26 16:43:39 +0400

    Update models.py

--- books/models.py
+++ books/models.py
@@ -1,3 +1,17 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+class Book(models.Model):
+    title = models.CharField()
+    description = models.TextField()
+    author = models.ManyToOneRel()
+    category = models.ManyToManyRel()
+    rating = models.IntegerField()
+
+
+class Category(models.Model):
+    name = models.CharField()
+
+
+class Author(models.Model):
+    name = models.CharField()
+    
